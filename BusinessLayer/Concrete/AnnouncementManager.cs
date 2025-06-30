63be8ad34b87fa177d5c9527019aae553e0fe4d8
yusuf-bilgin <yusufbilgin1905@gmail.com>
2025-06-30 18:25:11 +0300

    feat(business): implement IAnnouncementService and AnnouncementManager

---
+++ BusinessLayer/Concrete/AnnouncementManager.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BusinessLayer.Abstract;
+using EntityLayer.Concrete;
+
+namespace BusinessLayer.Concrete
+{
+    public class AnnouncementManager : IAnnouncementService
+    {
+        IAnnouncementService _announcementService;
+
+        public AnnouncementManager(IAnnouncementService announcementService)
+        {
+            _announcementService = announcementService;
+        }
+
+        public void TAdd(Announcement t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TDelete(Announcement t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Announcement TGetByID(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Announcement> TGetList()
+        {
+            return _announcementService.TGetList();
+        }
+
+        public void TUpdate(Announcement t)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
