92738e5d5686aa6bf537fcd16009e1c729b98859
yusuf-bilgin <yusufbilgin1905@gmail.com>
2025-07-04 00:34:54 +0300

    feat(writer): add ProfileController and profile edit view layout - Created ProfileController with Index action in Writer area. - Designed profile edit form view with input fields for name, surname, password, and profile image upload.

---
+++ AtoZProject/Areas/Writer/Controllers/ProfileController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AtoZProject.Areas.Writer.Controllers
+{
+    [Area("Writer")]
+    public class ProfileController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
