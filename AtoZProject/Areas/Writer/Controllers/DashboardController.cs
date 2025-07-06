b5b2b9b003a986c08fe06e889ad0c971a9f3c223
yusuf-bilgin <yusufbilgin1905@gmail.com>
2025-07-06 22:43:43 +0300

    feat(writer): add dashboard view with user info and statistics - Created DashboardController to display user-specific dashboard data - Added logic to fetch and show statistics like announcement and skill count - Designed dashboard view with welcome message and statistics cards

---
+++ AtoZProject/Areas/Writer/Controllers/DashboardController.cs
@@ -0,0 +1,40 @@
+﻿using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using System.Windows.Markup;
+using DataAccessLayer.Concrete;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AtoZProject.Areas.Writer.Controllers
+{
+    [Area("Writer")]
+    public class DashboardController : Controller
+    {
+        private readonly UserManager<WriterUser> _userManager;
+
+        public DashboardController(UserManager<WriterUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            if (!User.Identity.IsAuthenticated)  
+                return RedirectToAction("Login", "Account");
+
+            var user = await _userManager.FindByNameAsync(User.Identity.Name);
+            ViewBag.v = $"{user.Name ?? "İsim Yok"} {user.Surname ?? "Soyisim Yok"}";
+
+            // Istatistik verilerini çekip ViewBag'e atama
+            Context c = new Context();
+            ViewBag.v1 = 0;
+            ViewBag.v2 = c.Announcements.Count();
+            ViewBag.v3 = 0;
+            ViewBag.v4 = c.Skills.Count();
+
+            return View();
+        }
+    }
+}
