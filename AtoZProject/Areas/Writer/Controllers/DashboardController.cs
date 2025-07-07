d9618cdd08f531528667f41b5f5db576bc1d6812
yusuf-bilgin <yusufbilgin1905@gmail.com>
2025-07-07 17:07:50 +0300

    feat(writer): integrate OpenWeather API to show real-time temperature in dashboard - Added OpenWeather API integration to DashboardController to fetch current temperature for Antalya - Parsed XML response and extracted temperature value using LINQ to XML - Displayed temperature dynamically in dashboard view via ViewBag.weather

--- AtoZProject/Areas/Writer/Controllers/DashboardController.cs
+++ AtoZProject/Areas/Writer/Controllers/DashboardController.cs
@@ -2,6 +2,7 @@
 using System.Linq.Expressions;
 using System.Threading.Tasks;
 using System.Windows.Markup;
+using System.Xml.Linq;
 using DataAccessLayer.Concrete;
 using EntityLayer.Concrete;
 using Microsoft.AspNetCore.Identity;
@@ -27,6 +28,12 @@ namespace AtoZProject.Areas.Writer.Controllers
             var user = await _userManager.FindByNameAsync(User.Identity.Name);
             ViewBag.v = $"{user.Name ?? "İsim Yok"} {user.Surname ?? "Soyisim Yok"}";
 
+            // Weather API'den veri çekme
+            string apiKey = "e8e061a1db8e5021855a2678529d4cae";
+            string connection = "https://api.openweathermap.org/data/2.5/weather?q=antalya&appid=" + apiKey + "&units=metric&mode=xml"; // Celsius için units=metric, XML formatı için mode=xml 
+            XDocument document = XDocument.Load(connection);
+            ViewBag.weather = document.Descendants("temperature").FirstOrDefault()?.Attribute("value")?.Value ?? "Veri Bulunamadı";
+
             // Istatistik verilerini çekip ViewBag'e atama
             Context c = new Context();
             ViewBag.v1 = 0;
@@ -38,3 +45,5 @@ namespace AtoZProject.Areas.Writer.Controllers
         }
     }
 }
+
+// https://api.openweathermap.org/data/2.5/weather?q=antalya&appid=e8e061a1db8e5021855a2678529d4cae&units=metric&mode=xml
