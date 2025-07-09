2efb60c796d1aed0bbd141f84218059944e73e90
yusuf-bilgin <yusufbilgin1905@gmail.com>
2025-07-09 19:03:16 +0300

    feat(writer): implement notification dropdown with ViewComponent
    - Added Notification ViewComponent to display latest 5 announcements in navbar.
    - Updated _NavbarPartial.cshtml to invoke the component dynamically.
    - Created Default.cshtml view for Notification ViewComponent with color-coded badges based on status.

---
+++ AtoZProject/Areas/Writer/ViewComponents/Notification.cs
@@ -0,0 +1,17 @@
+﻿using System.Linq;
+using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AtoZProject.Areas.Writer.ViewComponents
+{
+    public class Notification : ViewComponent
+    {
+        AnnouncementManager announcementManager = new AnnouncementManager(new EfAnnouncementDal());
+        public IViewComponentResult Invoke()
+        {
+            var values = announcementManager.TGetList().Take(5).ToList();
+            return View(values);
+        }
+    }
+}
