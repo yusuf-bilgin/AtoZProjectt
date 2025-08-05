13bfeb71307ea9c178981227c5257c3c5342b6c2
yusuf-bilgin <yusufbilgin1905@gmail.com>
2025-08-05 14:39:35 +0300

    feat(admin/social-media): add social media management with add, edit, and list features
    - Added SocialMediaController and SocialMediaManager
    - Implemented SocialMediaList ViewComponent for dynamic rendering
    - Created views: AddSocialMedia, EditSocialMedia, Index
    - Updated NewSidebar.cshtml to include Social Media navigation
    - Updated FeatureList/Default.cshtml to integrate with shared SocialMediaList component

---
+++ AtoZProject/Controllers/SocialMediaController.cs
@@ -0,0 +1,52 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AtoZProject.Controllers
+{
+    public class SocialMediaController : Controller
+    {
+        SocialMediaManager socialMediaManager = new SocialMediaManager(new EfSocialMediaDal());
+
+        public IActionResult Index()
+        {
+            var values = socialMediaManager.TGetList();
+            return View(values);
+        }
+
+        [HttpGet]
+        public IActionResult AddSocialMedia()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult AddSocialMedia(SocialMedia p)
+        {
+            p.Status = true; // Default status set to true
+            socialMediaManager.TAdd(p);
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult DeleteSocialMedia(int id)
+        {
+            var value = socialMediaManager.TGetByID(id);
+            socialMediaManager.TDelete(value);
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult EditSocialMedia(int id)
+        {
+            var value = socialMediaManager.TGetByID(id);
+            return View(value);
+        }
+        [HttpPost]
+        public IActionResult EditSocialMedia(SocialMedia p)
+        {
+            socialMediaManager.TUpdate(p);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ AtoZProject/ViewComponents/SocialMedia/SocialMediaList.cs
@@ -0,0 +1,16 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AtoZProject.ViewComponents.SocialMedia
+{
+    public class SocialMediaList : ViewComponent
+    {
+        SocialMediaManager _socialMediaManager = new SocialMediaManager(new EfSocialMediaDal());
+        public IViewComponentResult Invoke()
+        {
+            var values = _socialMediaManager.TGetList();
+            return View(values);
+        }
+    }
+}
