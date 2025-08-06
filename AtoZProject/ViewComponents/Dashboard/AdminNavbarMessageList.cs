a5cf0cf3a29b34293322ce2beaa714e38cf8e4d5
yusuf-bilgin <yusufbilgin1905@gmail.com>
2025-08-06 12:37:06 +0300

    feat(admin/navbar): add message and notification components to admin navbar

---
+++ AtoZProject/ViewComponents/Dashboard/AdminNavbarMessageList.cs
@@ -0,0 +1,20 @@
+﻿using System.Linq;
+using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AtoZProject.ViewComponents.Dashboard
+{
+    public class AdminNavbarMessageList : ViewComponent
+    {
+        WriterMessageManager writerMessageManager = new WriterMessageManager(new EfWriterMessageDal());
+
+        public IViewComponentResult Invoke()
+        {
+            // Admin mailinin gönderdiği son 3 mesajı listele
+            string p = "admin@gmail.com";
+            var values = writerMessageManager.GetListReceiverMessage(p).OrderByDescending(x=>x.WriterMessageId).Take(3).ToList();
+            return View(values);
+        }
+    }
+}
---
+++ AtoZProject/ViewComponents/Dashboard/AdminNavbarNotificationList.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AtoZProject.ViewComponents.Dashboard
+{
+    public class AdminNavbarNotificationList : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+
+            return View();
+        }
+    }
+}
