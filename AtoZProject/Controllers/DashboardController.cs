04e44e07f601e7ab74a93fd1b2c2dd8f628d6375
yusuf-bilgin <yusufbilgin1905@gmail.com>
2025-08-06 12:48:42 +0300

    fix(dashboard): resolved script conflict causing UI malfunction

--- AtoZProject/Controllers/DashboardController.cs
+++ AtoZProject/Controllers/DashboardController.cs
@@ -6,9 +6,6 @@ namespace AtoZProject.Controllers
     {
         public IActionResult Index()
         {
-            ViewBag.v1 = "Dashboard";
-            ViewBag.v2 = "İstatistikler";
-            ViewBag.v3 = "İstatistik Sayfası";
             return View();
         }
     }
