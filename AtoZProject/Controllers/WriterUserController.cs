25d7877d8b90d54e00c7dc85804f7a8a31f4899a
yusuf-bilgin <yusufbilgin1905@gmail.com>
2025-08-09 22:40:22 +0300

    chore(writer-user): add initial controller, view, service, and DAL structure for AJAX feature
    - Created WriterUserController and initial Index.cshtml view
    - Added service (IWriterUserService, WriterUserManager) in BusinessLayer
    - Added data access interfaces and EF implementation (IWriterUserDal, EfWriterUserDal)

---
+++ AtoZProject/Controllers/WriterUserController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AtoZProject.Controllers
+{
+    public class WriterUserController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
