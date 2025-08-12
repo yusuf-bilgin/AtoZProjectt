c2ad4be10af9149362b12a1ce9ccbd519e71e8ee
yusuf-bilgin <yusufbilgin1905@gmail.com>
2025-08-12 17:18:03 +0300

    feat(experience2): implement AJAX list, add, and get-by-id operations
    - Added Experience2Controller with Index, ListExperience, AddExperience, and GetById actions
    - Implemented AJAX calls in Index view for fetching all, adding new, and retrieving experience by ID
    - Updated view to display dynamic data in a table format

---
+++ AtoZProject/Controllers/Experience2Controller.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace AtoZProject.Controllers
+{
+    public class Experience2Controller : Controller
+    {
+        ExperienceManager _experienceManager = new ExperienceManager(new EfExperienceDal());
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult ListExperience()
+        {
+            var values = _experienceManager.TGetList();
+            return Json(JsonConvert.SerializeObject(values));
+        }
+
+        [HttpPost]
+        public IActionResult AddExperience(Experience p)
+        {
+            _experienceManager.TAdd(p);
+            var values = JsonConvert.SerializeObject(p);
+            return Json(values);
+        }
+
+        [HttpPost]
+        public IActionResult GetById(int id)
+        {
+            var result = _experienceManager.TGetByID(id);
+            return View(JsonConvert.SerializeObject(result));
+        }
+    }
+}
